917e89c00c848c4063afd46649c93430eb944f9c
phyark <992060446@qq.com>
2021-04-08 18:21:45 +0800

    update: 完善用户模型

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,8 +2,17 @@ const DataTypes = require('sequelize')
 const sequelize = require('./connection')
 
 const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.STRING(50),
+    primaryKey: true
+  },
   username: DataTypes.STRING,
-  password: DataTypes.STRING
+  password: DataTypes.STRING,
+  createTime: DataTypes.BIGINT,
+  lastTime: DataTypes.BIGINT,
+  version: DataTypes.BIGINT
+}, {
+  timestamps: false,
 })
 
 module.exports = User
