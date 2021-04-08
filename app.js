e103311fe55a248668b0523e88e49f817964dd64
phyark <992060446@qq.com>
2021-04-08 18:22:21 +0800

    update: 增加首页静态模板

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const Router = require('./routes/index')
@@ -7,7 +8,7 @@ const Router = require('./routes/index')
 // 解析json格式的body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(path.join(__dirname, '/public')))
 // 监听路由
 Router(app)
 
