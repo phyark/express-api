cddacbcbbf98726c6cfc37960f593695af668736
phyark <phyark@hotmail.com>
2023-01-20 10:44:14 +0800

    feat: test

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,3 +1,17 @@
 (function () {
-  console.log(document)
+    const username = document.querySelector('input[name=username]')
+    const password = document.querySelector('input[name=password]')
+    const loginBtn = document.querySelector('.btn')
+    fetch()
+
+    const handleLogin = () => {
+        console.log({ username: username.value, password: password.value })
+    }
+
+    const resetForm = () => {
+        username.value = ''
+        password.value = ''
+    }
+
+    loginBtn.addEventListener('click', handleLogin)
 })()
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const UserController = require('../controllers/UserController')
 const userConllor = new UserController()
 
 // 注册
-router.post('/register', userConllor.register.bind(userConllor))
+router.get('/register', userConllor.register.bind(userConllor))
 
 // 登录
 router.post('/login', userConllor.login.bind(userConllor))
