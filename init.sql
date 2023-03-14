e78b55da21b83948b3b2d6be7ebf65e3040ba7cf
phyark <phyark@hotmail.com>
2023-03-14 23:39:50 +0800

    chore: format sql comment

--- init.sql
+++ init.sql
@@ -1,10 +1,10 @@
-/* 在本地创建 www 用户密码为 www */
+-- 在本地创建 www 用户密码为 www
 CREATE user 'www' @'%' identified by 'www';
 
-/* 赋予本地 www 用户 在 admin_server 库，全权限  */
+-- 赋予本地 www 用户 在 admin_server 库，全权限
 GRANT all privileges on admin_server.* to 'www' @'%' with grant option;
 
-/* 更新权限配置 */
+-- 更新权限配置
 flush privileges;
 
 DROP DATABASE IF EXISTS `admin_server`;
@@ -13,7 +13,7 @@ CREATE DATABASE `admin_server`;
 
 Use `admin_server`;
 
-/* 创建用户表 */
+-- 创建用户表
 CREATE TABLE `Users`(
   `id` INT UNSIGNED AUTO_INCREMENT KEY COMMENT '用户ID',
   `phone` VARCHAR(20) comment "手机号",
@@ -25,5 +25,5 @@ CREATE TABLE `Users`(
   `version` BIGINT DEFAULT NULL comment "版本"
 ) ENGINE = "INNODB" DEFAULT CHARSET = "UTF8";
 
-/* 插入数据 */
+-- 插入数据
 INSERT INTO users(username,password) VALUES ('admin', '123456');
